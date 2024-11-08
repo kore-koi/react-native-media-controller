452478702c991b6ac4249681aa6ac1f2514ca0b0
Andrea <andrea.lin3110@gmail.com>
2024-11-08 21:42:59 +0100

    Remove Swift Volume Value Guard
    As any value is valid for the volume, the guard is removed

--- ios/HybridMPVolumeViewController.swift
+++ ios/HybridMPVolumeViewController.swift
@@ -11,10 +11,6 @@ class HybridMPVolumeViewController: HybridMPVolumeViewControllerSpec {
     func setVolume(params: MPVolumeViewControllerSetVolumeParams) throws -> Void {
         NSLog("HybridMPVolumeViewController.setVolume(volume:%f) is being called", params.volume)
         
-        guard params.volume >= 0 && params.volume <= 1 else {
-            throw NSError(domain: "HybridMPVolumeViewController", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid Volume Value, must be between 0 and 1"])
-        }
-        
         MPVolumeView.setVolume(Float(params.volume))
     }
 }
